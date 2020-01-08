8a289855a662c5d62867dd31d34ee4a7bc2a9f31
长青 <wuxd@global.com>
2020-01-08 16:39:26 +0800

    构建一个框架

---
+++ thread1/src/main/java/com/yunji/ThreadProjectApplication.java
@@ -0,0 +1,16 @@
+package com.yunji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author changqing
+ * @date 2020-01-08 15:46
+ * @description:
+ */
+@SpringBootApplication
+public class ThreadProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadProjectApplication.class,args);
+    }
+}
